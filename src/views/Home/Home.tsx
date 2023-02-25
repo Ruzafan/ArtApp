a699290c12b4af51a14802dd5e8722785408fe9f
Ruzafan <marc.ramiro@gmail.com>
2023-02-25 21:22:04 +0100

    get a list of art pieces

--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -3,9 +3,10 @@ import { artService } from "services/http";
 import styles from "./Home.module.scss";
 import ArtDetail from "../../component/ArtDetail/ArtDetail";
 const Home = () => {
-  let [artTitle, setArtTitle] = useState<string>("");
-  let [imageUrl, setImageUrl] = useState<string>("");
-  artService
+  // let [artTitle, setArtTitle] = useState<string>("");
+  //let [imageUrl, setImageUrl] = useState<string>("");
+  let [artList, setArtList] = useState<Array<any>>();
+  /*artService
     .get("artworks/129884")
     .then((res) => {
       console.log(res);
@@ -18,16 +19,37 @@ const Home = () => {
     .catch(console.error)
     .finally(() => {
       //setLoading(false)
-    });
+    });*/
+  function loadImages() {
+    artService
+      .get("artworks")
+      .then((res) => {
+        console.log(res.data.data);
+        let list: Array<any> = [];
+        res.data.data.map((info: any) => {
+          let imgUrl: string = `https://www.artic.edu/iiif/2/${info.image_id}/full/250,/0/default.jpg`;
+          let title: string = info.artist_display;
+          let artDetail = ArtDetail({ imgUrl, title });
+          console.log(artDetail);
+          list.push(artDetail);
+        });
+        setArtList(list);
+      })
+      .catch(console.error)
+      .finally(() => {
+        //setLoading(false)
+      });
+  }
 
   return (
     <>
       <div className={styles.Home}>Home</div>
-      <div className={styles.artList}>
-        <ArtDetail imgUrl={imageUrl} title={artTitle} />
-      </div>
+      <button onClick={loadImages}>Load images</button>
+      <div className={styles.artList}>{artList}</div>
     </>
   );
 };
 //"artworks?page=2&limit=100"
+//<ArtDetail imgUrl={imageUrl} title={artTitle} />
+
 export default Home;
